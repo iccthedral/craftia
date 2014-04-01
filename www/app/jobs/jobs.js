c748da02214058d38b7e21d4752f78fbf6c265a3
Aleksandar Milic <nullpotent@gmail.com>
2014-04-01 04:32:06 +0200

    job photos and google maps

--- www/app/jobs/jobs.js
+++ www/app/jobs/jobs.js
@@ -120,6 +120,40 @@
         $scope.dropped = function(dragEl, dropEl) {
             
         }
+        
+        window["initializeGmaps"] = function(data) {
+            console.debug(data);
+              var mapOptions = {
+                zoom: 8,
+                center: new google.maps.LatLng(-34.397, 150.644)
+              };
+
+              var map = new google.maps.Map(document.getElementById('map-canvas'),
+                  mapOptions);
+        }
+
+        $scope.initLeftPartial = function () {
+            $.ajax({
+                url: 'http://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyC8x39I4Aa4jYbZuTGkoGlFm5Cm4sZD1E0&sensor=false&' + 
+                'callback=initializeGmaps',
+                dataType: "jsonp",
+                jsonp: "initializeGmaps",
+                success: function(data) {
+                    alert('tu ')
+                    // console.debug(data);
+                }
+            });
+
+            // function initialize() {
+            //   var mapOptions = {
+            //     zoom: 8,
+            //     center: new google.maps.LatLng(-34.397, 150.644)
+            //   };
+
+            //   var map = new google.maps.Map(document.getElementById('map-canvas'),
+            //       mapOptions);
+            // }
+        }
 
         function JobList() {
             $scope.focusedMap = {};
